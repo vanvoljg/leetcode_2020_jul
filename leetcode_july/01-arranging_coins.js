22c5ef91115aadfdd7dd51e85241948f2eed09d5
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-07-01 16:33:26 -0700

    algo complete
    math works every time

--- leetcode_july/01-arranging_coins.js
+++ leetcode_july/01-arranging_coins.js
@@ -5,7 +5,12 @@
  * @return {number}
  */
 const arrangeCoins = (n) => {
-  return 0;
+  // This can be solved using math
+  // n = the area of sum(1 to x), and we return floor(x)
+  // n = x*(x+1)/2 -> x = (sqrt(8n+1)-1)/2
+  // Return floor of this
+  let x = Math.floor((Math.sqrt(8 * n + 1) - 1) / 2);
+  return x;
 };
 
 const testRunner = (tests, func) => {
@@ -24,8 +29,11 @@ const testRunner = (tests, func) => {
 
 const test = () => {
   const tests = [
+    { input: 1, expected: 1 },
+    { input: 3, expected: 2 },
     { input: 5, expected: 2 },
     { input: 8, expected: 3 },
+    { input: 15, expected: 5 },
   ];
 
   testRunner(tests, arrangeCoins);
