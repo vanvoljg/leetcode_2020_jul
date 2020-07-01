b945587d3203a47fdfa96b8b7f30a8646486fdfc
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-07-01 13:07:12 -0700

    add tests and runner

--- leetcode_july/01-arranging_coins.js
+++ leetcode_july/01-arranging_coins.js
@@ -5,5 +5,30 @@
  * @return {number}
  */
 const arrangeCoins = (n) => {
-  
+  return 0;
 };
+
+const testRunner = (tests, func) => {
+  const name = func.name;
+  for (let { input, expected } of tests) {
+    const result = func(input);
+    console.log({
+      name,
+      input,
+      result,
+      expected,
+      pass: result === expected,
+    });
+  }
+};
+
+const test = () => {
+  const tests = [
+    { input: 5, expected: 2 },
+    { input: 8, expected: 3 },
+  ];
+
+  testRunner(tests, arrangeCoins);
+};
+
+export default test();
