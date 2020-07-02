c46a8fc6958887734750bbb7291790d9f78c7db0
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-07-02 12:21:18 -0700

    add tree and deque classes

---
+++ leetcode_july/lib/deque.js
@@ -0,0 +1,82 @@
+export class QueueNode {
+  constructor(val = null) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+export class Queue {
+  constructor() {
+    this.front = null;
+    this.back = this.front;
+    this.length = 1;
+  }
+  
+  // Add to back
+  push(val) {
+    const node = new QueueNode(val);
+    if (this.length === 0) {
+      this.front = node;
+      this.back = this.front;
+    } else {
+      node.prev = this.back;
+      this.back.next = node;
+      this.back = node;
+    }
+    this.length++;
+  }
+
+  // Add to front
+  unshift(val) {
+    const node = new QueueNode(val);
+    if (this.length === 0) {
+      this.back = node;
+      this.front = this.back;
+    } else {
+      node.next = this.front;
+      this.front.prev = node;
+      this.front = node;
+    }
+    this.length++;
+  }
+  
+  // Remove from front
+  shift() {
+    if (this.front) {
+      const node = this.front;
+      this.front = this.front.next;
+      this.front.prev = null;
+      this.length--;
+      return node.val;
+    } else {
+      return null;
+    }
+  }
+
+  // Remove from back
+  pop() {
+    if (this.back) {
+      const node = this.back;
+      this.back = this.back.prev;
+      this.back.next = null;
+      this.length--;
+      return node.val;
+    } else {
+      return null;
+    }
+  }
+  
+  // Return front value
+  peek() {
+    return this.front !== null ? this.front.val : null;
+  }
+
+  // Return back value
+  peekTail() {
+    return this.back !== null ? this.back.val : null;
+  }
+
+
+
+}
